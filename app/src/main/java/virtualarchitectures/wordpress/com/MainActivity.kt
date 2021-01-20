4934709f1a86bb22a038c49cbec48dff9ce67592
DESKTOP-RHUN1EM\olive <virtualarchitectures@gmail.com>
2021-01-20 20:19:22 +0000

    Initial commit

---
+++ app/src/main/java/virtualarchitectures/wordpress/com/MainActivity.kt
@@ -0,0 +1,11 @@
+package virtualarchitectures.wordpress.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
